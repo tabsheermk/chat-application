fc2794fd00146289755936344638f6852ef45456
tabsheermk <creativemk12@gmail.com>
2025-05-23 12:48:29 +0530

    proper dir structure setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello, world!");
+});
+
 app.listen(3003, () => {
   console.log("App is running on PORT 3003");
 });
